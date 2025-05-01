f3e403a668b2ea6809bc4280a0798437146009d8
tsgamage <thevidu2007627@gmail.com>
2025-05-01 09:29:13 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong Game")
+
+turtle = Turtle()
+
+
+screen.exitonclick()
