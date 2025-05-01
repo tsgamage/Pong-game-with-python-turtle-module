60bf05bc4f08385328d9c63570d4f53d2e2bf3b6
tsgamage <thevidu2007627@gmail.com>
2025-05-01 13:29:38 +0530

    Make the ball bounce and detect if ball hits the top and bottom and paddles. Reset ball position if ball missed by a paddle

--- ball.py
+++ ball.py
@@ -1,4 +1,4 @@
-import random
+import time
 from turtle import Turtle
 
 class Ball(Turtle):
@@ -7,5 +7,27 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.penup()
+        self.x_move = 1
+        self.y_move = 1
 
+    def move(self,):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x,new_y)
 
+    def bounce_y(self):
+        self.y_move  *= -1
+
+    def bounce_x(self):
+        self.x_move  *= -1
+
+    def missed(self, side):
+        self.teleport(0,0)
+
+        if side.lower() == "right":
+            self.x_move = -1
+            self.y_move = -1
+
+        if side.lower() == "left":
+            self.x_move = -1
+            self.y_move = -1
--- main.py
+++ main.py
@@ -9,8 +9,8 @@ screen.bgcolor("black")
 screen.title("Pong Game")
 screen.tracer(0)
 
-right_paddle = Paddle(360,0)
-left_paddle = Paddle(-370,0)
+right_paddle = Paddle(360, 0)
+left_paddle = Paddle(-370, 0)
 
 ball = Ball()
 
@@ -26,10 +26,28 @@ while _game_running:
     time.sleep(0.005)
     screen.update()
 
+    ball.move()
+
+    # Delect if the ball hits the top and bottom
+    if ball.ycor() > 285 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect if the ball hits the paddle
+    collapse_with_right_paddle = 335 < ball.xcor() < 355 and ball.distance(right_paddle) < 50
+    collapse_with_left_paddle = -355 > ball.xcor() > -375 and ball.distance(left_paddle) < 50
+
+    if collapse_with_right_paddle or collapse_with_left_paddle:
+        ball.bounce_x()
 
 
+    # Detect if the ball missed by a paddle
+    ball_pass_right_paddle = ball.xcor() > 400
+    ball_pass_left_paddle = ball.xcor() < -400
 
+    if ball_pass_right_paddle:
+        ball.missed("right")
 
-
+    if ball_pass_left_paddle:
+        ball.missed("left")
 
 screen.exitonclick()
