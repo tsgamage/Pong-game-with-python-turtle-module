6d3da4ec2f1903aa673042f880d0c961ec376f80
tsgamage <thevidu2007627@gmail.com>
2025-05-01 10:52:05 +0530

    Make both paddle control from keys cause a screen refresh error

--- main.py
+++ main.py
@@ -6,13 +6,22 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
+screen.tracer(0)
 
-right_paddle = Paddle(-390,0)
+right_paddle = Paddle(360,0)
+left_paddle = Paddle(-370,0)
+
+screen.listen()
+
+screen.onkeypress(left_paddle.go_up, "w")
+screen.onkeypress(left_paddle.go_down, "s")
+screen.onkeypress(right_paddle.go_up, "Up")
+screen.onkeypress(right_paddle.go_down, "Down")
 
 _game_running = True
 while _game_running:
-    time.sleep(0.1)
-    right_paddle.move(-240,240)
+    screen.update()
+
 
 
 
--- paddles.py
+++ paddles.py
@@ -1,5 +1,7 @@
-from turtle import Turtle
+import time
+from turtle import Turtle, Screen
 
+screen = Screen()
 
 class Paddle(Turtle):
     def __init__(self, x, y, paddle_color = "white"):
@@ -7,12 +9,24 @@ class Paddle(Turtle):
         self.y = y
         self.x = x
         self.shape("square")
-        self.shapesize(5,1)
+        self.shapesize(1,5)
+        self.setheading(90)
         self.color(paddle_color)
         self.penup()
         self.teleport(x,y)
         self.speed("slowest")
 
     def move(self,y_from, y_to):
-        self.goto(self.x,y_from)
-        self.goto(self.x,y_to)
+        self.goto(self.x, y_from)
+        self.goto(self.x, y_to)
+
+
+    def go_up(self, distance=10):
+        current_y = self.ycor()
+        new_y = current_y + distance
+        self.goto(self.x,new_y)
+
+    def go_down(self, distance=10):
+        current_y = self.ycor()
+        new_y = current_y - distance
+        self.goto(self.x, new_y)
